35f32466901eafe6c7c2b8ed598fa5b369b09932
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-08-10 20:55:26 -0400

    Implemented JS functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,125 @@
 $(document).ready(function () {
+
+    var total = 0;
+    var wins = 0;
+    var losses = 0;
  
-    var randomNumber = Math.floor(Math.random() * 102) + 19;
+var randomNumber = Math.floor(Math.random() * 101) + 19;
 
-    console.log(randomNumber);
+ console.log(randomNumber);
 
-    $("#random").append("#crystal1");
-
-
-$("#crystal1").on("click", function () {
+ $("#randomNumber").text(randomNumber);
 
 
-})
+    var numberOne = Math.floor(Math.random() * 11) + 1;
+    var numberTwo = Math.floor(Math.random() * 11) + 1;
+    var numberThree = Math.floor(Math.random() * 11) + 1;
+    var numberFour = Math.floor(Math.random() * 11) + 1;
+
+    
+
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
 
 });
 
-var numberOne = Math.floor(Math.random() * 12) + 1;
-var numberTwo = Math.floor(Math.random() * 12) + 1;
-var numberThree = Math.floor(Math.random() * 12) + 1;
-var numberFour = Math.floor(Math.random() * 12) + 1;
+function reset () {
+
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    $("randomNumber").text(randomNumber);
+    var numberOne = Math.floor(Math.random() * 11) + 1;
+    var numberTwo = Math.floor(Math.random() * 11) + 1;
+    var numberThree = Math.floor(Math.random() * 11) + 1;
+    var numberFour = Math.floor(Math.random() * 11) + 1;
+
+    total = 0;
+    $("#score").text(total);
+
+}
+
+// Need to add number of wins to the total score "score"
+
+function won () {
+    alert( "Congratulations you won!");
+
+    win++;
+    $("#numberWins").text(wins);
+
+    reset();
+}
+
+function loss () {
+
+    alert("Sorry you've loss");
+    losses++;
+    $("#numberLosses").text(losses);
+
+    reset();
+}
+
+$("#crystal1").on("click", function () {
+
+    total = total + numberOne;
+
+    $("#score").text(total);
+
+    if(total === randomNumber){
+
+        won();
+    }
+
+    else if (total > randomNumber){
+        loss ();
+    }
+});
+
+$("#crystal2").on("click", function () {
+
+    total = total + numberTwo;
+
+    $("#score").text(total);
+
+    if(total === randomNumber){
+
+        won();
+    }
+
+    else if(total > randomNumber) {
+        loss ();
+    }
+
+})
+
+$("#crystal3").on("click", function () {
+
+    total = total + numberThree;
+
+    $("#score").text(total);
+
+    if(total === randomNumber){
+
+        won();
+    }
+
+    else if (total > randomNumber){
+        loss ();
+    }
+
+})
+
+$("#crystal4").on("click", function () {
+
+    total = total + numberFour;
+
+    $("#score").text(total);
+
+    if(total === randomNumber){
+
+        won();
+    }
+
+    else if (total > randomNumber){
+        loss ();
+    }
+});
+
