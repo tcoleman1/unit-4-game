4d592a1b1bd50ec694bd41d89c8ff98ab240f982
Tanzania Coleman <tanzania.coleman@gmail.com>
2019-08-10 20:09:49 -0400

    Updated css and html files

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+ 
+    var randomNumber = Math.floor(Math.random() * 102) + 19;
+
+    console.log(randomNumber);
+
+    $("#random").append("#crystal1");
+
+
+$("#crystal1").on("click", function () {
+
+
+})
+
+});
+
+var numberOne = Math.floor(Math.random() * 12) + 1;
+var numberTwo = Math.floor(Math.random() * 12) + 1;
+var numberThree = Math.floor(Math.random() * 12) + 1;
+var numberFour = Math.floor(Math.random() * 12) + 1;
